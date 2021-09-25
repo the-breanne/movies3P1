a763b423fea09c7b506a1027e09d0cde859b7385
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-09-25 09:08:42 -0500

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR, 'static')
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
