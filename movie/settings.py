4caf7f03282eb31ea54488f455b5cc9093f4ea02
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-09-25 08:44:16 -0500

    update allowed hosts and debug

--- movie/settings.py
+++ movie/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m@4sna%!pjo^@v#wcn0s&p5^n*y%i7tnd2ynjq-9dkxwx&fe$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
